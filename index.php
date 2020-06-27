82e9499358408bcfe8bfbcd924251a8396041234
Thomas <thomas.brun.perso@gmail.com>
2020-06-27 03:16:13 +0200

    Ajout de la page accueil

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-Hello
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title>Hôtel Monge - St André les Alpes</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<link rel="icon" type="image/x-icon" href="img/general/icon.png" />
+	</head>
+	
+	<!-- Body -->
+	<body>
+        <div id="menu">
+            <ul>
+                <li class="page-active"><a href="index.php">Accueil</a></li>
+                <li><a href="views/presentation.php">Présentation</a></li>
+            </ul>
+        </div>
+	
+		<div id="wrapper">
+            <h1>Bienvenue sur Perler Imagiro</h1>
+            <hr />
+		</div>
+
+		<!-- Footer -->
+		<div id="footer">
+			<p>
+				Edité par Brun Thomas,<br />
+				2013 - <?php echo date('Y') ?>
+			</p>
+		</div>
+	</body>
+</html>
